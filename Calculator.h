0a01b3e605fb262b552bda7832d69bd84419356e
Ruchita Sankhat <ruchitasankhat@gmail.com>
2025-07-21 08:40:06 +0530

    Update Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -1,6 +1,36 @@
-#include"calculator.h"
+#include <iostream>
+using namespace std;
 
-int main() {
-        show();
-        return 0;
+inline void show() {
+        double a,b;
+        char op;
+
+        cout<<"Enter First Number:";
+        cin>>a;
+        cout<<"Enter Second Number:";
+        cin>>b;
+        cout<<"Enter Operator(+,-,*,/):";
+        cin>>op;
+
+        switch(op) {
+                case '+':
+                        cout<<"Addition:"<<a+b;
+                        break;
+                case '-':
+                        cout<<"Subtraction:"<<a-b;
+                        break;
+                case '*':
+                        cout<<"Multiplication:"<<a*b;
+                        break;
+                case '/':
+                        if(b!=0) {
+                                cout<<"Division:"<<a/b;
+                        }else{
+                                cout<<"Invalid Division!";
+                        }
+                        break;
+                default:
+                    cout<<"Error!Invalid Operator";        
+                        }
+
 }
